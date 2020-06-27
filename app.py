f1764fb034784990f38c7db0728afec523fdd63e
MarcosAP20 <aguiar.marcos20@gmail.com>
2020-06-27 10:51:02 -0300

    Interação com Heroku.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from flask_cors import CORS
 from config.model import configure as config_db
 from flask_migrate import Migrate
 from config.serealizer import configure as config_ma
@@ -8,6 +9,14 @@ from blueprint.validacao_compra_bp import validacao_compra_bp
 from blueprint.produto_bp import  produto_bp
 from blueprint.pedidos_bp import pedidos_bp
 app=Flask(__name__)
+
+cors = CORS(app, resource={r"/*": {"origins": "*"}})
+
+@app.route("/", methods=['GET'])
+def main():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 DB_URL = 'postgresql+psycopg2://{user}:{passw}@{port}/{db}'.format(user="postgres", passw="paidegua;", port="localhost", db="postgres")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
@@ -25,5 +34,5 @@ app.register_blueprint(validacao_compra_bp)
 app.register_blueprint(produto_bp)
 app.register_blueprint(pedidos_bp)
 
-if __name__=="__main__":
-    app.run(port=5000)
+if __name__ == "__main__":
+    main()
