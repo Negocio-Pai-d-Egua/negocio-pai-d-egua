d34d1379e59b0e3ea668440a2287f2288c32281a
MarcosAP20 <aguiar.marcos20@gmail.com>
2020-06-27 11:52:56 -0300

    Interação com Heroku(requirementos corrigido).

--- app.py
+++ app.py
@@ -14,10 +14,15 @@ app = Flask(__name__)
 
 cors = CORS(app, resource={r"/*": {"origins": "*"}})
 
-@app.route("/", methods = ['GET'])
+@app.route("/deploy", methods = ['GET'])
+def deploy():
+    return "<h1>Testando deploy Github e Heroku</h1>"
+
 def main():
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
+
+
 
 DB_URL = 'postgresql+psycopg2://{user}:{passw}@{port}/{db}'.format(user="postgres", passw="paidegua;", port="localhost", db="postgres")
 
