7534ac871a8c339a9b48a2d6a569e1ce089bcf86
MarcosAP20 <aguiar.marcos20@gmail.com>
2020-06-28 11:15:01 -0300

    Correção: botão "limpar carrinho"

--- blueprint/validacao_compra_bp.py
+++ blueprint/validacao_compra_bp.py
@@ -6,11 +6,10 @@ from requests import post
 validacao_compra_bp = Blueprint("validacao_compra", __name__, template_folder="templates")
 
 
-@validacao_compra_bp.route("/validacao_compra", methods=["post"])
+@validacao_compra_bp.route("/validacao_compra",methods= ["post"])
 def validacao_compra():
-    carrinho= request.cookies.get("mesa") or None
+    carrinho= request.cookies.get("mesa") or request.form["mesa"]
     produto= request.form["produto_id"] or request.cookies.get("produto_id")
-    print(carrinho)
     if carrinho:
         carrinho= Carrinho.query.filter(Carrinho.id==int(carrinho)).first()
         produto= Produto.query.filter(Produto.id==int(produto)).first()
@@ -23,7 +22,7 @@ def validacao_compra():
                 return redirect(url_for("index.home"))
         novo_produto = {"nome": produto.nome, "preco": produto.preco, "quantidade": 1, "image": produto.image}
         id_novoproduto = criar_produto(novo_produto)
-        produto = Produto.query.filter(Produto.id == id_novoproduto).first()
+        produto= Produto.query.filter(Produto.id==id_novoproduto).first()
         preco_total = float(carrinho.totalpreco) + float(produto.preco)
         carrinho.totalpreco = str(preco_total)
         carrinho.produtos.append(produto)
@@ -31,7 +30,9 @@ def validacao_compra():
         return redirect(url_for("index.home"))
 
     else:
-        return render_template("formulario.html")
+        response = make_response(redirect(url_for("carrinho.create_carrinho", carrinho=carrinho), code=307))
+        response.set_cookie("produto_id", produto)
+        return response
 
 
     return "erro"
@@ -63,3 +64,10 @@ def pedido_finalizado():
 @validacao_compra_bp.route("/formulario", methods= ["post"])
 def formulario():
     return render_template("pedido_finalizado.html")
+
+@validacao_compra_bp.route("/salvando_cookie", methods= ["post"])
+def salvando_cookie():
+    mesa = request.form["mesa"]
+    response = make_response(redirect(url_for("carrinho.validacao_compra"), code=307))
+    response.set_cookie("mesa", mesa)
+    return response
