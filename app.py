636c738fc854acec6a359e6df4e0488a714ce188
Marcelofilho97 <marcelosfilho1997@gmail.com>
2020-06-28 21:09:20 -0300

    Ultimos detalhes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from blueprint.index_bp import index_bp
 from blueprint.carrinho_bp import carrinho_bp
 from blueprint.validacao_compra_bp import validacao_compra_bp
 from blueprint.produto_bp import  produto_bp
-from blueprint.pedidos_bp import pedidos_bp
+
 
 app=Flask(__name__)
 DB_URL = 'postgresql+psycopg2://{user}:{passw}@{port}/{db}'.format(user="postgres", passw="12131415", port="localhost", db="negocio")
@@ -24,7 +24,7 @@ app.register_blueprint(index_bp)
 app.register_blueprint(carrinho_bp)
 app.register_blueprint(validacao_compra_bp)
 app.register_blueprint(produto_bp)
-app.register_blueprint(pedidos_bp)
+
 
 if __name__=="__main__":
     app.run(port=5000)
